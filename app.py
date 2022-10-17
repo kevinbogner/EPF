28b820ab504abe5661f37e2faf784943a249609f
kevinbogner <kev.bogner@gmail.com>
2022-10-17 19:57:40 +0200

    modify data + add pie charts

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import plotly.express as px
 #Streamlit Configs
 st.set_page_config(page_title="Data Analysis Consensus Clients",
                    page_icon=":floppy_disk:",
-                   layout="wide")
+                   #layout="wide"
+                   )
 
 st.title('💾Data Analysis of Consensus Clients')
 st.markdown('The aim is to provide a **Data Analysis of the Consensus Clients landscape to statistically identify inconsistency, seeking to improve the Consensus Layer.**')
@@ -28,19 +29,29 @@ def load_relay30d_data(nrows):
     return data
 
 
+
 #Load data
 slotclientreward_data = load_slotclientreward_data(100)
 relay7d_data = load_relay7d_data(100)
 relay30d_data = load_relay30d_data(100)
 
 
-#Chart
-st.subheader('Chart')
-
 #BarChart
+st.subheader('Consensus Clients Distribution')
 fig = px.bar(slotclientreward_data, x = 'Client', y = 'Number of Slots', color='Number of Slots', color_continuous_scale='Viridis')
 st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
+
+#PieChart 7Days
+st.subheader('Relay Distribution Last 7 Days')
+fig = px.pie(relay7d_data, values='BlockCount', names='Name')
+st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+
+
+#PieChart 7Days
+st.subheader('Relay Distribution Last 30 Days')
+fig = px.pie(relay30d_data, values='BlockCount', names='Name')
+st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 
 #Data
