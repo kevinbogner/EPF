b1a8df4a10b957d707b47552fe331450ba45320b
kevinbogner <kev.bogner@gmail.com>
2022-10-18 14:39:15 +0200

    data cleanup + new charts

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ st.set_page_config(page_title="Data Analysis Consensus Clients",
                    #layout="wide"
                    )
 
+
+#Overview
 st.title('💾Data Analysis of Consensus Clients')
 st.markdown('Data provided by [blockprint](https://github.com/sigp/blockprint) and [beaconcha.in](https://beaconcha.in/). Thank You:heart:')
 st.markdown('Last Updated on Slot: 4895291')
@@ -30,28 +32,45 @@ def load_relay30d_data(nrows):
     return data
 
 
-
 #Load data
 slotclientreward_data = load_slotclientreward_data(100)
 relay7d_data = load_relay7d_data(100)
 relay30d_data = load_relay30d_data(100)
 
 
-#BarChart
-st.subheader('Consensus Clients Distribution')
-fig = px.bar(slotclientreward_data, x = 'Client', y = 'Number of Slots', color='Number of Slots', color_continuous_scale='Viridis')
+#Consensus Clients
+st.subheader('Consensus Clients')
+
+#Consensus Clients - Distribution
+fig = px.pie(slotclientreward_data, values='Number of Slots', names='Client', title='Distribution')
+st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+
+#Consensus Clients - Rewards/Slot
+fig = px.bar(slotclientreward_data, x = 'Client', y = 'Average Reward/Slot', color='Client', title='Rewards/Slot')
 st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 
-#PieChart 7Days
-st.subheader('Relay Distribution Last 7 Days')
-fig = px.pie(relay7d_data, values='BlockCount', names='Name')
+#Relay Last 7 Days
+st.subheader('Relay Last 7 Days')
+
+#Relay Last 7 Days - Distribution
+fig = px.pie(relay7d_data, values='BlockCount', names='Name', title='Distribution')
+st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+
+#Relay Last 7 Days - Rewards/Slot
+fig = px.bar(relay7d_data, x = 'Name', y = 'AverageReward', color='Name', title='Rewards/Slot')
 st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 
-#PieChart 7Days
-st.subheader('Relay Distribution Last 30 Days')
-fig = px.pie(relay30d_data, values='BlockCount', names='Name')
+#Relay Last 30 Days
+st.subheader('Relay Last 30 Days')
+
+#Relay Last 30 Days - Distribution
+fig = px.pie(relay30d_data, values='BlockCount', names='Name', title='Distribution')
+st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+
+#Relay Last 30 Days - Rewards/Slot
+fig = px.bar(relay30d_data, x = 'Name', y = 'AverageReward', color='Name', title='Rewards/Slot')
 st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 
