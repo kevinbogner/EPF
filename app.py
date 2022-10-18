014f0b9458e73fea99b90668ce52f15e852d3455
kevinbogner <kev.bogner@gmail.com>
2022-10-18 16:53:32 +0200

    cleaner layout + comments in code

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-#Streamlit Configs
+#---Streamlit Configs---
 st.set_page_config(page_title="Data Analysis Consensus Clients",
                    page_icon=":floppy_disk:",
-                   #layout="wide"
+                   layout="wide"
                    )
 
 
-#Overview
+#---Overview---
 st.title('💾Data Analysis of Consensus Clients')
 st.markdown('Data provided by [blockprint](https://github.com/sigp/blockprint) and [beaconcha.in](https://beaconcha.in/). Thank You:heart:')
 st.markdown('Last Updated on Slot: 4895291')
 
 
-#Import data from .csv
+#---Import data from .csv---
 @st.cache
 def load_slotclientreward_data(nrows):
     data = pd.read_csv('./data/slotclientreward.csv', nrows=nrows)
@@ -32,59 +32,77 @@ def load_relay30d_data(nrows):
     return data
 
 
-#Load data
+#---Load data---
 slotclientreward_data = load_slotclientreward_data(100)
 relay7d_data = load_relay7d_data(100)
 relay30d_data = load_relay30d_data(100)
 
 
-#Consensus Clients
+#---Consensus Clients---
 st.subheader('Consensus Clients')
 
+col1, col2 = st.columns(2)
+
 #Consensus Clients - Distribution
-fig = px.pie(slotclientreward_data, values='Number of Slots', names='Client', title='Distribution')
-st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+with col1:
+    fig = px.pie(slotclientreward_data, values='Number of Slots', names='Client', title='Distribution')
+    st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 #Consensus Clients - Rewards/Slot
-fig = px.bar(slotclientreward_data, x = 'Client', y = 'Average Reward/Slot', color='Client', title='Rewards/Slot')
-st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+with col2:
+    fig = px.bar(slotclientreward_data, x = 'Client', y = 'Average Reward/Slot', color='Client', title='Rewards/Slot')
+    st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 
-#Relay Last 7 Days
+#---Relay Last 7 Days---
 st.subheader('Relay Last 7 Days')
 
+col1, col2 = st.columns(2)
+
 #Relay Last 7 Days - Distribution
-fig = px.pie(relay7d_data, values='BlockCount', names='Name', title='Distribution')
-st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+with col1:
+    fig = px.pie(relay7d_data, values='BlockCount', names='Name', title='Distribution')
+    st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
-#Relay Last 7 Days - Rewards/Slot
-fig = px.bar(relay7d_data, x = 'Name', y = 'AverageReward', color='Name', title='Rewards/Slot')
-st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+#Relay Last 7 Days - Distribution
+with col2:
+    fig = px.bar(relay7d_data, x = 'Name', y = 'AverageReward', color='Name', title='Rewards/Slot')
+    st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 
-#Relay Last 30 Days
+#---Relay Last 30 Days----
 st.subheader('Relay Last 30 Days')
 
+col1, col2 = st.columns(2)
+
 #Relay Last 30 Days - Distribution
-fig = px.pie(relay30d_data, values='BlockCount', names='Name', title='Distribution')
-st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+with col1:
+    fig = px.pie(relay30d_data, values='BlockCount', names='Name', title='Distribution')
+    st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 #Relay Last 30 Days - Rewards/Slot
-fig = px.bar(relay30d_data, x = 'Name', y = 'AverageReward', color='Name', title='Rewards/Slot')
-st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+with col2:
+    fig = px.bar(relay30d_data, x = 'Name', y = 'AverageReward', color='Name', title='Rewards/Slot')
+    st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 
-#Data
+#---Data---
 st.subheader('Data')
-if st.checkbox('Slot Client Reward'):
-    st.write(slotclientreward_data)
 
-if st.checkbox('Relay 7d'):
-    st.write(relay7d_data)
+col1, col2, col3 = st.columns(3)
 
-if st.checkbox('Relay 30d'):
-    st.write(relay30d_data)
+with col1:
+    if st.checkbox('Slot Client Reward'):
+        st.write(slotclientreward_data)
+
+with col2:
+    if st.checkbox('Relay 7d'):
+        st.write(relay7d_data)
+
+with col3:
+    if st.checkbox('Relay 30d'):
+        st.write(relay30d_data)
 
 
-#Footer
+#---Footer---
 st.subheader('Footer')
